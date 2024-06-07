f57d69ef6710cc70131e712b4a032500e233615e
bill <coderbarn@gmail.com>
2024-06-07 09:35:35 -0400

    Added ViewAllJobs.jsx

--- vite-project-1/src/App.jsx
+++ vite-project-1/src/App.jsx
@@ -2,7 +2,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
- 
+import ViewAllJobs from "./components/ViewAllJobs";
+
 const App = () => {
   // Can only return a single element but you can have as much
   //    in that element that you want
@@ -23,15 +24,9 @@ const App = () => {
     <Hero title="Become a React Dev Today!" subtitle="Find the React job that fits your skills and needs"/>
     <HomeCards />
     <JobListings />
-    
+    <ViewAllJobs />
 
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+
 
    </>
   );
