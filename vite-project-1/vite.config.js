711e017e920876a6346cb2fc99654301f6a2a90d
bill <coderbarn@gmail.com>
2024-06-07 17:12:56 -0400

    Added server code and proxy

--- vite-project-1/vite.config.js
+++ vite-project-1/vite.config.js
@@ -7,5 +7,13 @@ export default defineConfig({
   // added the following
   server: {
     port: 3000,
+    proxy:{
+      '/api':{
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+
+    }
   },
 })
