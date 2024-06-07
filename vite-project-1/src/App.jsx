ab928354c711f28cf120b3db74538a2953d0947f
bill <coderbarn@gmail.com>
2024-06-07 17:31:31 -0400

    Added job page

--- vite-project-1/src/App.jsx
+++ vite-project-1/src/App.jsx
@@ -7,6 +7,7 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage from './pages/JobPage';
 
 const router = createBrowserRouter(
 
@@ -14,6 +15,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />} >
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/jobs/:id' element={<JobPage />} />
         <Route path='*' element={<NotFoundPage />} />
     </Route>
     
